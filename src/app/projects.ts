cf1913acaadee02b0d7f23dff95fd8f4af2a095f
Michael <micumieu@gmail.com>
2017-08-08 14:31:17 -0700

    complete projects section

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -1,35 +1,43 @@
 const projects = [
     {
       name: 'nicefinds',
-      description: 'Lorem ipsum dolor sit amet, dicant civibus persecuti no ius. Et mediocrem erroribus argumentum mea, exerci adipisci splendide ex eos, ea usu blandit imperdiet quaerendum. Mel ea rebum iudico numquam. Ut mel option integre, sed ex dico utamur ullamcorper. Alii legimus cu has, munere dissentiet ei usu.',
+      description: "An amazon affiliate-based website designed for efficiency. Features a simple to use, yet powerful admin panel. Ability to publish products from amazon to the main page with an affiliate link. Products can be published manually or with a direct link and a click of a button, be automatically posted on the front page with it's name, brand, product description, pictures, a tweet sent out with a photo, and saved to a database. Front page features ability to sort by categories, tag, or brand name, and infinite scrolling.",
       link: 'http://www.nicefinds.net/',
       imgUrl: 'https://s24.postimg.org/qebts6yrp/logo_favicon.png',
-      technologies: [ 'Angular', 'MongoDB', 'Express', 'Node.js', 'Bootstrap', 'HTML', 'CSS'],
+      technologies: [ 'Angular', 'MongoDB', 'Express', 'Node.js', 'Bootstrap', 'HTML', 'CSS', 'Javascript'],
       apis: ['Amazon Product API', 'Twitter API', 'Bitly API', 'Webshot API']
     },
     {
       name: 'learndevanagari',
-      description: 'Id tempor altera fabulas usu. Augue tamquam pertinacia duo ut, duo cu error nominati explicari, eum mollis aliquando reprimique te. Eam ei facer facete. Magna mediocrem cu sea, agam reformidans ut est.',
+      description: 'A simple and fun app to learn Nepali (devanagari script). Features a simple user interface to test characters in study mode, account creation to save progress and stats, leaderboards to see how others compare, and a learn section to get an overview of the script. Also features an admin panel to add, edit, and/or remove characters for future revision or expansion.',
       link: 'https://learndev.herokuapp.com/',
       imgUrl: 'https://s9.postimg.org/topy52rm7/Screen_Shot_2017-03-10_at_1.01.31_PM.png',
-      technologies: [ 'Angular', 'MongoDB', 'Express', 'Node.js', 'Bootstrap', 'HTML', 'CSS' ],
+      technologies: [ 'Angular', 'MongoDB', 'Express', 'Node.js', 'Bootstrap', 'HTML', 'CSS', 'Javascript'],
       apis: ['Gravatar API', 'Twitter API']
     },
     {
       name: 'ngportfolio',
-      description: 'At laudem persequeris qui, vis molestiae adipiscing at, liberavisse comprehensam mei ad. Nonumy tibique duo ut, at corpora adversarium per, pro in nostrum aliquando. Ad nam oblique tibique molestiae. Qui tation homero labitur ad, vim eu doctus pericula. Erant tation conclusionemque in per.',
+      description: 'This website. Features a user-experience reminiscent to that of a native iPhone or Android app. Features Angular Animations for smooth transitions between different routes.',
       link: 'https://michaelcom.herokuapp.com/',
       imgUrl: 'https://s17.postimg.org/6ytcmy4zj/Copy_of_logo_favicon_ngportfolio.png',
-      technologies: [ 'Angular', 'Angular Animations', 'Express', 'Node.js', 'Bootstrap', 'HTML', 'CSS'],
+      technologies: [ 'Angular', 'Angular Animations', 'Express', 'Node.js', 'Bootstrap', 'HTML', 'CSS', 'Javascript'],
       apis: ['Mailgun API']
     },
     {
       name: 'indeed-me',
-      description: 'At laudem persequeris qui, vis molestiae adipiscing at, liberavisse comprehensam mei ad. Nonumy tibique duo ut, at corpora adversarium per, pro in nostrum aliquando. Ad nam oblique tibique molestiae. Qui tation homero labitur ad, vim eu doctus pericula. Erant tation conclusionemque in per.',
+      description: "A side-project to quickly get relevant data in a tabular format from indeed's website.",
       link: 'https://github.com/micustudio/indeed-me',
       imgUrl: 'https://s1.postimg.org/f29uzcdb3/ngportfolio_logo_favicon_ngportfolio.png',
-      technologies: [ 'Angular', 'Express', 'Node.js', 'Bootstrap', 'HTML', 'CSS'],
+      technologies: [ 'Angular', 'Express', 'Node.js', 'Bootstrap', 'HTML', 'CSS', 'Javascript'],
       apis: ['Indeed Api']
+    },
+    {
+      name: 'travel-site',
+      description: 'A flexible and customizable single page brochure site built with a modern development workflow in mind.',
+      link: 'https://micustudio.github.io/travel-site/#',
+      imgUrl: 'https://s2.postimg.org/rznwg2czd/Screen_Shot_2017-08-08_at_1.32.15_PM.png',
+      technologies: [ 'HTML', 'CSS', 'Javascript', 'JQuery', 'Gulp', 'PostCSS', 'BEM Philosophy', 'Webpack' ],
+      apis: []
     }
   
   ]
