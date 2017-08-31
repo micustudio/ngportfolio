ee6440009f1f0fb1be0b2d2f284de93dbf226b25
Michael <micumieu@gmail.com>
2017-08-30 22:02:58 -0700

    slight adjustment to project description

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -33,7 +33,7 @@ const projects = [
     },
     {
       name: 'travel-site',
-      description: 'A flexible and customizable single page (brochure) site built with modern javascript, tools, and best practices.',
+      description: 'A flexible and customizable single page (brochure) site built with modern developer skills, tools, and best practices.',
       link: 'https://micustudio.github.io/travel-site/#',
       imgUrl: 'https://s2.postimg.org/rznwg2czd/Screen_Shot_2017-08-08_at_1.32.15_PM.png',
       technologies: [ 'HTML', 'CSS', 'Javascript', 'jQuery', 'Gulp', 'PostCSS', 'BEM Philosophy', 'Webpack' ],
